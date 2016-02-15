d293844a551204b0162ccd2d03ca69edc43418f8
zhangyunrui <635798274@qq.com>
2016-02-15 17:16:50 +0800

    modify zen_of_python

--- zen_of_python.py
+++ zen_of_python.py
@@ -25,6 +25,4 @@ for c in (65, 97):
 	for i in xrange(26):
 		d[chr(i+c)] = chr((i+13) % 26 + c)
 
-# print "".join([d.get(c, c) for c in s])
-# print "".join([d.get(c) for c in s])
-print d.get('c', 'd')
+print "".join([d.get(c, c) for c in s])
